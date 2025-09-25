223fee3594f0fe8e22f7bee82a1fad032b0ac0dc
Bgleb73 <bgleb73@gmail.com>
2025-09-25 21:39:57 +0400

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,6 +11,7 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
@@ -19,11 +20,40 @@ export default ((opts?: Options) => {
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
-            <li>
+            <li key={text}>
               <a href={link}>{text}</a>
             </li>
           ))}
         </ul>
+
+        {/* Вставка кода Яндекс.Метрики через dangerouslySetInnerHTML */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(103367752, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103367752"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </footer>
     )
   }
